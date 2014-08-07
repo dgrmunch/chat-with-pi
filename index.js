9afa71daf4d3eee2b448049cfb0af70ae3e2ccd5
Diego González Rodríguez <xmunch@xmunch.com>
2014-08-08 00:34:32 +0200

    Change port to 80

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var express = require('express'),
 	currentDirectory = '',
     nicknames = {};
 
-server.listen(8000);
+server.listen(80);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
