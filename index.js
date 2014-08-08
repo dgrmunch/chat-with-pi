ab3ca3486542b86f8000d3feb5c16730b07340e8
Diego González Rodríguez <xmunch@xmunch.com>
2014-08-08 12:52:16 +0200

    Basic implementation of helper agent

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var express = require('express'),
 	currentDirectory = '',
     nicknames = {};
 
-server.listen(8000);
+server.listen(5000);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
@@ -33,6 +33,7 @@ io.sockets.on('connection', function(socket) {
             callback(true);
             socket.nickname = data;
             nicknames[socket.nickname] = 1;
+			launchHelperWelcome();
             updateNickNames();
         }
     });
@@ -47,12 +48,21 @@ io.sockets.on('connection', function(socket) {
 		if(!socket.nickname) return;
 		callback(true);
 		console.log(socket.nickname + ' is going to execute a command');
-		if(getCurrentDirectory() === ''){
-			console.log('Directory needs to be setted');
-			pwd(executeCommand,dataIn);
+		if( dataIn.substring(0,1) === '/') {
+			if( dataIn === '/help') {
+			io.sockets.emit('new message', {msg:'<b>Command &nbsp<span class="good-command">'+dataIn+'</span></b> was executed.', nick: socket.nickname});
+			launchHelperInfo();
+			} else{
+				io.sockets.emit('new message', {msg: '<b>Command &nbsp<span class="bad-command">'+dataIn+"</span></b> was rejected.", nick: socket.nickname});
+			}
 		} else {
-			console.log('Directory already setted');
-			executeCommand(dataIn);
+			if(getCurrentDirectory() === ''){
+				console.log('Directory needs to be setted');
+				pwd(executeCommand,dataIn);
+			} else {
+				console.log('Directory already setted');
+				executeCommand(dataIn);
+			}
 		}
     });
 	
@@ -71,11 +81,16 @@ io.sockets.on('connection', function(socket) {
 		console.log('exectuteCommand() was called');
 		seq = 'cd '+ getCurrentDirectory() + '\n' + dataIn + '\npwd';
 		console.log('This sequence of commands will be executed:\n' + seq);
-  	  	child = exec(seq, function(error, stdout, stderr) {
-			console.log('With this results:\n'+stdout);
-			dataOut = processStdout(stdout);
-			returnCommandResults(dataIn,dataOut);
-  	  	});
+		if(dataIn === 'ls' || dataIn === 'ps aux' || dataIn === 'cat README.md' || (dataIn.split(" ")[0] === 'cd') && dataIn.split(" ").length === 2) {
+		io.sockets.emit('new message', {msg: '<b>Command &nbsp<span class="good-command">'+dataIn+'</span></b> was executed.', nick: socket.nickname});
+child = exec(seq, function(error, stdout, stderr) {
+	console.log('With this results:\n'+stdout);
+	dataOut = processStdout(stdout);
+	returnCommandResults(dataIn,dataOut);
+});
+		} else{
+			io.sockets.emit('new message', {msg: '<b> Command &nbsp<span class="bad-command">'+dataIn+'</span></b> was rejected. <span class="good-command">The only allowed unix commands in this demo version are <b>cd</b>, <b>ls</b>, <b>ps aux</b> and <b>cat README.md</b>.</span>', nick: socket.nickname});
+		}
 	}
     
     function updateNickNames() {
@@ -109,5 +124,14 @@ io.sockets.on('connection', function(socket) {
 		setCurrentDirectory(array[array.length - 2]);
 		array.splice(array.length - 2, 1);
 		return array.join('\n');
+	}
+	
+	function launchHelperWelcome(){
+		nicknames['raspberry-pi'] = 1; //User raspberri-pi connected by default
+		io.sockets.emit('new message', {msg: '<h4>Hi '+socket.nickname+'!</h4> Welcome to <b>chat-with-pi</b>! Do you need some help to start? Write <b>/help</b> and click the <b>send command</b> button (&nbsp<span class="glyphicon glyphicon-screenshot"></span>&nbsp).', nick: 'raspberry-pi'});
+	}
+	
+	function launchHelperInfo(){
+		io.sockets.emit('new message', {msg: '<b>chat-with-pi</b> is an user-friendly interface to communicate with a <b>Raspberry pi</b> like me ;-)<br> In this web interface you can send commands, browse directories, play with GPIO... <b>Whatever you want!</b>.<br> Oriented to education, <b>chat-with-pi</b> allows collective learning. Students and professors (or geeks) can join the conversation at any moment. They only need a browser to practise.<br>Whatever you do, it will be shared on real-time with all the connected users. You can use the textbox to:<ul><li><b>Send a message (&nbsp<span class="glyphicon glyphicon-send"></span>&nbsp): </b> You can share your knowledge teaching how to control the <b>Raspberry pi</b>, ask/answer a technical question, tell a friend what you are going to do,... </li><li><b>Send a command (&nbsp<span class="glyphicon glyphicon-screenshot"></span>&nbsp): </b> You will control your <b>Raspberry pi</b> with a remote console.</li></ul>', nick: 'raspberry-pi'});
 	}
 });
