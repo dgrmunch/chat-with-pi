c34d42a2113e509b36733b9b5eb1114782b18d24
Diego González Rodríguez <xmunch@xmunch.com>
2014-08-08 13:03:35 +0200

    Add process.env.PORT

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ var express = require('express'),
     io = require("socket.io").listen(server),
 	path = require('path'),
 	currentDirectory = '',
-    nicknames = {};
-
-server.listen(5000);
+    nicknames = {},
+	port = Number(process.env.PORT || 5000);
+	
+server.listen(port);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
